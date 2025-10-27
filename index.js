acf669cf72d47163b45e72d263d87d7fa6472523
Idowu Olabanji <idowuolabanji181@gmail.com>
2025-10-27 15:44:26 +0100

    ttt

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {"message": "Type in your URL:", 
+    "name": "url"}
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
